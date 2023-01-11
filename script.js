e7d0607ea2fbed3b8522e3f656584123d587d8b6
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-10 16:50:54 -0800

    Add button and change createGrid function to be triggered by button click

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+/*
 const userInput = prompt("Enter a number");
+*/
 
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    const userInput = prompt("Enter a number");
+    for (let i = 0; i < userInput; i++) {
+        const container = document.querySelector('#container');
+        const row = document.createElement('div');
+        row.setAttribute('id', 'row');
+        container.appendChild(row);
+        for (let i = 0; i < userInput; i++) {
+            const box = document.createElement('div');
+            box.setAttribute('id', 'box');
+            box.addEventListener('mouseover', (e) => {
+                e.target.style.background = 'red';
+            });
+            row.appendChild(box);
+        }
+    }
+    });
+
+/*
 function createGrid() {
+    
     for (let i = 0; i < userInput; i++) {
         const container = document.querySelector('#container');
         const row = document.createElement('div');
@@ -19,3 +42,4 @@ function createGrid() {
 
 createGrid();
 
+*/
