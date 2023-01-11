4298e27210fa926983a240dee56c5e425a618eab
DinoCzar <116979569+DinoCzar@users.noreply.github.com>
2023-01-10 17:00:55 -0800

    Change grid size in style.css and delete old comments in script.js

--- script.js
+++ script.js
@@ -1,45 +1,23 @@
-/*
-const userInput = prompt("Enter a number");
-*/
-
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
     const userInput = prompt("Enter a number");
-    for (let i = 0; i < userInput; i++) {
-        const container = document.querySelector('#container');
-        const row = document.createElement('div');
-        row.setAttribute('id', 'row');
-        container.appendChild(row);
+    if (userInput < 100) {
         for (let i = 0; i < userInput; i++) {
-            const box = document.createElement('div');
-            box.setAttribute('id', 'box');
-            box.addEventListener('mouseover', (e) => {
-                e.target.style.background = 'red';
-            });
-            row.appendChild(box);
+            const container = document.querySelector('#container');
+            const row = document.createElement('div');
+            row.setAttribute('id', 'row');
+            container.appendChild(row);
+            for (let i = 0; i < userInput; i++) {
+                const box = document.createElement('div');
+                box.setAttribute('id', 'box');
+                box.addEventListener('mouseover', (e) => {
+                    e.target.style.background = 'red';
+                });
+                row.appendChild(box);
+            }
         }
+    } else {
+        return alert("Please enter a number less than 100");
     }
-    });
+});
 
-/*
-function createGrid() {
-    
-    for (let i = 0; i < userInput; i++) {
-        const container = document.querySelector('#container');
-        const row = document.createElement('div');
-        row.setAttribute('id', 'row');
-        container.appendChild(row);
-        for (let i = 0; i < userInput; i++) {
-            const box = document.createElement('div');
-            box.setAttribute('id', 'box');
-            box.addEventListener('mouseover', (e) => {
-                e.target.style.background = 'red';
-            });
-            row.appendChild(box);
-        }
-    }
-}
-
-createGrid();
-
-*/
